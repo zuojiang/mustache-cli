cf1c928649aeadf8132db08feba2d7ebbcbbecbc
nadou <499140448@139.com>
2017-07-11 15:20:51 +0800

    add --conf and --out options

--- bin/cli.js
+++ bin/cli.js
@@ -1,43 +1,77 @@
 #!/usr/bin/env node
 
 var Path = require('path')
-var program = require('commander');
-var stdin = require('stdin');
-var pkg = require('../package.json');
-var core = require('../lib/index');
+var fs = require('fs')
+var program = require('commander')
+var stdin = require('stdin')
+var mkdirp = require('mkdirp')
+var pkg = require('../package.json')
+var core = require('../lib/index')
 
 var cwd = process.cwd()
 
-program.name('mustache');
-program.version(pkg.version);
+program.name('mustache')
+program.version(pkg.version)
 program.usage('[options] [dir]')
-program.option('-h, --help', 'output usage information');
-program.option('-m, --minify', 'compile minify HTML output');
-program.option('-s, --silent', 'do not output logs');
-program.option('-w, --watch', 'watch files');
-program.option('-c, --conf-dir <dir>', 'the config file directory (*.js or *.json, Default: ./conf)');
-program.option('-t, --tpl-dir <dir>', 'template file directory (Default: ./tpl)');
-program.option('-o, --out-dir <dir>', 'output the rendered HTML or compiled JavaScript to <dir> (Default: ./out)');
-program.option('-e, --extension <ext>', 'specify the output file extension (Default: html)');
-program.option('--pipe', 'enables pipeline mode');
-program.option('--color', 'enables colors on the console');
-program.option('--root-tpl <str>', 'as an entry to compile (Default: __root)');
-program.option('--tpl-prefix <str>', 'as a loading partial from a tpl file (Default: __)');
-program.option('--partial-prefix <str>', 'as a string partial (e.g. {{>partial}}, Default: _)');
-program.option('--global-data <js|json>', 'the file as global data to compile');
-program.parse(process.argv);
+program.option('-h, --help', 'output usage information')
+program.option('-m, --minify', 'compile minify HTML output')
+program.option('-s, --silent', 'do not output logs')
+program.option('-w, --watch', 'watch files')
+program.option('-c, --conf-dir <dir>', 'the config file directory (*.js or *.json, Default: ./conf)')
+program.option('-t, --tpl-dir <dir>', 'template file directory (Default: ./tpl)')
+program.option('-o, --out-dir <dir>', 'output the rendered HTML or compiled JavaScript to <dir> (Default: ./out)')
+program.option('-e, --extension <ext>', 'specify the output file extension (Default: html)')
+program.option('-C, --conf <path>', 'the config file (*.js or *.json)')
+program.option('-O, --out <path>', 'the rendered HTML file (*.html)')
+program.option('--pipe', 'enables pipeline mode')
+program.option('--color', 'enables colors on the console')
+program.option('--root-tpl <str>', 'as an entry to compile (Default: __root)')
+program.option('--tpl-prefix <str>', 'as a loading partial from a tpl file (Default: __)')
+program.option('--partial-prefix <str>', 'as a string partial (e.g. {{>partial}}, Default: _)')
+program.option('--global-data <path>', 'the file as global data to compile (*.js or *.json)')
+program.parse(process.argv)
 program.on('--help', function(){
-  console.log('\nExamples:\n');
-  console.log('   mastche --color ./');
-  console.log('   cat data.json | mastche --pipe > index.html');
-  console.log('');
-});
+  console.log('\nExamples:\n')
+  console.log('   mastche --color ./')
+  console.log('   mastche -C data.json -O index.html ./')
+  console.log('   cat data.json | mastche --pipe > index.html')
+  console.log('')
+})
 
 if (program.help) {
-  var data = program.globalData ? require(Path.join(cwd, program.globalData)) : null;
+  var data = program.globalData ? require(Path.join(cwd, program.globalData)) : null
   core.setGlobalData(data)
 
-  if (program.pipe) {
+  if (program.conf) {
+    (function(){
+      var config = require(Path.resolve(program.conf))
+      var text = core.output({
+        baseDir: program.args[0],
+        confDir: program.confDir,
+        tplDir: program.tplDir,
+        outDir: program.outDir,
+        rootTpl: program.rootTpl,
+        tplPrefix: program.tplPrefix,
+        partialPrefix: program.partialPrefix,
+        print: program.silent ? function(){} : console.log,
+        onError: program.silent ? function(){} : console.error,
+        minify: program.minify,
+        watch: false,
+        color: false,
+        ext: program.extension,
+        variables: data,
+        config: config,
+      })
+      if (program.out) {
+        var outFile = Path.resolve(program.out)
+        mkdirp.sync(Path.dirname(outFile))
+        fs.writeFileSync(outFile, text)
+        console.log(outFile)
+      } else {
+        console.log(text)
+      }
+    })()
+  } else if (program.pipe) {
       stdin(function(json) {
         var config = JSON.parse(json.toString())
         var text = core.output({
@@ -50,12 +84,12 @@ if (program.help) {
           partialPrefix: program.partialPrefix,
           print: function(){},
           onError: function(e){
-            process.stderr.write(e.toString());
+            process.stderr.write(e.toString())
           },
           minify: program.minify,
           watch: false,
           color: false,
-          ext: undefined,
+          ext: program.extension,
           variables: data,
           config: config,
         })
